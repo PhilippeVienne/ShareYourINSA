681af6c901dd97391503fb15b915aa145810215f
Philippe Vienne <philippe@vienne.me>
2016-05-11 11:08:13 +0200

    Edit Dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,12 +3,24 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+
+    columns do
+      column do
+        panel "Info" do
+          para "Bienvenu sur la gestion de ShareYourINSA. Ce site est une démo hors production !"
+        end
+      end
+      column do
+        panel "Users" do
+          ul do
+            User.all.limit(5).map do |u|
+              li link_to(u.email, admin_user_path(u))
+            end
+          end
+        end
       end
     end
+
 
     # Here is an example of a simple dashboard with columns and panels.
     #
