b3fa3fb4f8fb207eb550ea60234de04064fdb879
Philippe Vienne <philippe.vienne@insa-lyon.fr>
2016-05-22 19:24:49 +0200

    Improve profile layout

--- app/models/profile_user.rb
+++ app/models/profile_user.rb
@@ -2,7 +2,7 @@ class ProfileUser < ActiveRecord::Base
   belongs_to :promotion
   belongs_to :user
 
-  has_attached_file :avatar, styles: {medium: '300x300>', thumb: '100x100>'}
+  has_attached_file :avatar, styles: {medium: '300x300>', thumb: '100x100>'}, default_url: 'https://static.licdn.com/scds/common/u/images/themes/katy/ghosts/person/ghost_person_60x60_v1.png'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
 end
