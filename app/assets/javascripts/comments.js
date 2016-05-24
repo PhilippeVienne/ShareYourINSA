f8c65392554a1945f5eece116fb4a89bac0dc9ca
Philippe Vienne <philippe@vienne.me>
2016-05-24 11:14:41 +0200

    AJAXify comments and put in a helper

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -4,11 +4,20 @@
 $(function(){
     $('.create-comment-form').submit(function(e){
 
-        var post_id = $(e.target).data('post-id');
-        var content = $(e.target).find('input').val();
+        var $form = $(e.target);
+        var $comments = $form.parent().find('.comments');
+        var post_id = $form.data('post-id');
+        var content = $form.find('input').val();
 
-        $.post('/posts/'+post_id+'/comments', {comment:{content: content}}, function(){
-            window.location.href = window.location.href;
+        $.post('/posts/'+post_id+'/comments', {comment:{content: content}}, function(result){
+            var $clone = $comments.find('.comment.model.hide').clone();
+            $clone.removeClass('hide');
+            $clone.find('.first_name').text(result.user.first_name);
+            $clone.find('.last_name').text(result.user.last_name);
+            $clone.find('.content').text(result.content);
+            $clone.find('img').attr('src',result.user.avatar);
+            $clone.appendTo($comments);
+            $form.find('input').val('');
         });
 
         e.preventDefault();
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def comments_for(post)
+    render 'application/comments', locals: {post:@post}
+  end
+
 end
