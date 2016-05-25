2ae963d34560ceee184d4a9150941b2f4fc88ff2
Philippe Vienne <philippe@vienne.me>
2016-05-25 08:28:28 +0200

    Add link to users profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
     # GET /users/:id.:format
     def show
-      # authorize! :read, @user
+      redirect_to controller: :profile, action: :show, id: params[:id]
     end
 
       # GET /users/:id/edit
@@ -22,20 +22,6 @@ class UsersController < ApplicationController
       else
         format.html { render action: 'edit' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-      # GET/PATCH /users/:id/finish_signup
-  def finish_signup
-    # authorize! :update, @user
-    if request.patch? && params[:user] #&& params[:user][:email]
-      if @user.update!(user_params)
-        @user.skip_reconfirmation!
-        sign_in(@user, :bypass => true)
-        redirect_to @user, notice: 'Your profile was successfully updated.'
-      else
-        @show_errors = true
       end
     end
   end
